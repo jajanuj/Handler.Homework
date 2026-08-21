e609e835b326f99690913ea1f56a4bdffbd7ef6d
James Chan <poyu.chan@allring-tech.com.tw>
2026-08-22 05:59:38 +0800

    Fix .gitignore ARM pattern swallowing Proc/Arm source folder
    [Aa][Rr][Mm]/ and [Aa][Rr][Mm]64/ in the VS gitignore template matched the
    real source folder Proc/Arm/ (Proc_ASM_Arm.cs, Proc_Sort_Arm.cs), which had
    been silently untracked since git init. Removed the two ARM platform-output
    patterns (this project targets .NET Framework 4.0, never builds ARM) and
    added the two previously-ignored files.

---
+++ ArtEQ/2_Function(流程)/Proc/Arm/Proc_ASM_Arm.cs
@@ -0,0 +1,108 @@
+﻿using ArtEQ._2_Function_流程_.BaseProc;
+using static ArtData.clsEnum;
+using Axis = ArtData.clsEnum.enuAxis;
+using Di = ArtData.clsEnum.enuDi;
+using Do = ArtData.clsEnum.enuDo;
+
+namespace ArtEQ._2_Function_流程_.Proc
+{
+    /// <summary>
+    /// 組裝手臂
+    /// </summary>
+    public class Proc_ASM_Arm : BaseArm
+    {
+        #region ===================== Singleton 設置 =====================
+
+        public static Proc_ASM_Arm GetSingleton() => GetSingletonInstance(() => new Proc_ASM_Arm("ASM_Arm"));
+
+        #endregion
+
+        protected Proc_ASM_Arm(string p_strName) : base(p_strName)
+        {
+            PickLane = Proc_HS_Lane.GetSingleton();
+            PlaceLane = Proc_ASM_Lane.GetSingleton();
+        }
+        protected override void BindHardwarePoint()
+        {
+            m_DI_Vacuum = Di.ASM_Arm_Vacuum;
+            m_DO_Vacuum_Activate = Do.ASM_Arm_Vacuum;
+            m_DO_Vacuum_Break = Do.ASM_Arm_Air;
+
+            m_Motor_X = Axis.ASM_Arm_X;
+            m_Motor_Y = Axis.ASM_Arm_Y;
+            m_Motor_Z = Axis.ASM_Arm_Z;
+
+        }
+
+        protected override bool ReadyToPick()
+        {
+            return GetPickLane().m_Temp_Tray_Info.bIsExist && GetPickLane().ArrivalSignal;
+        }
+
+        protected override BaseLane GetPickLane()
+        {
+            BaseLane pickLane = null;
+            switch (PPStation)
+            {
+                case ArtData.clsEnum.PPStation.IC:
+                    if (m_pickPlace == PickPlace.Pick)
+                    {
+                        pickLane = Proc_ASM_Lane.GetSingleton();
+                    }
+                    break;
+                case ArtData.clsEnum.PPStation.None:
+                case ArtData.clsEnum.PPStation.HeatSink:
+                    if (m_pickPlace == PickPlace.Pick)
+                    {
+                        pickLane = Proc_HS_Lane.GetSingleton();
+                    }
+                    break;
+            }
+
+            return pickLane;
+        }
+
+        protected override BaseLane GetPlaceLane()
+        {
+            BaseLane placeLane = null;
+            switch (PPStation)
+            {
+                case ArtData.clsEnum.PPStation.None:
+                case ArtData.clsEnum.PPStation.IC:
+                    if (m_pickPlace == PickPlace.Place)
+                    {
+                        placeLane = Proc_ASM_Lane.GetSingleton();
+                    }
+                    break;
+                case ArtData.clsEnum.PPStation.HeatSink:
+                    if (m_pickPlace == PickPlace.Place)
+                    {
+                        placeLane = Proc_HS_Lane.GetSingleton();
+                    }
+                    break;
+            }
+
+            return placeLane;
+        }
+
+        protected override bool ReadyToPlace()
+        {
+            return GetPlaceLane().m_Temp_Tray_Info.bIsExist && GetPlaceLane().ArrivalSignal;
+        }
+
+        protected override void TransferToLane()
+        {
+            var placeLane = GetPlaceLane();
+            if (placeLane == null)
+                return;
+
+            var index = placeLane.m_Temp_Tray_Info.GetIndexFromRowCol(m_iPlaceRow, m_iPlaceColumn);
+
+            AssyRecord.CopyTo(placeLane.m_Temp_Tray_Info.AssyRecords[index]);
+            placeLane.m_Temp_Tray_Info.AssyRecords[index].IsAssembled = true;
+            placeLane.m_Temp_Tray_Info.AssyRecords[index].CurrentStation = WorkStationType.ASM;
+            AssyRecord.IsExist = false;
+            placeLane.m_Temp_Tray_Info.SetItemStatus(index, TrayItemStatus.Assembly);
+        }
+    }
+}
---
+++ ArtEQ/2_Function(流程)/Proc/Arm/Proc_Sort_Arm.cs
@@ -0,0 +1,109 @@
+﻿using ArtData;
+using ArtEQ._2_Function_流程_.BaseProc;
+using Axis = ArtData.clsEnum.enuAxis;
+using Di = ArtData.clsEnum.enuDi;
+using Do = ArtData.clsEnum.enuDo;
+
+namespace ArtEQ._2_Function_流程_.Proc
+{
+    /// <summary>
+    /// 組裝手臂
+    /// </summary>
+    public class Proc_Sort_Arm : BaseArm
+    {
+        #region ===================== Singleton 設置 =====================
+
+        public static Proc_Sort_Arm GetSingleton() => GetSingletonInstance(() => new Proc_Sort_Arm("Sort_Arm"));
+
+        #endregion
+
+        protected Proc_Sort_Arm(string p_strName) : base(p_strName)
+        {
+        }
+
+        protected override void BindHardwarePoint()
+        {
+            m_DI_Vacuum = Di.Sort_Arm_Vacuum;
+            m_DO_Vacuum_Activate = Do.Sort_Arm_Vacuum;
+            m_DO_Vacuum_Break = Do.Sort_Arm_Air;
+
+            m_Motor_X = Axis.Sort_Arm_X;
+            m_Motor_Y = Axis.Sort_Arm_Y;
+            m_Motor_Z = Axis.Sort_Arm_Z;
+
+        }
+
+        protected override bool ReadyToPick()
+        {
+            return GetPickLane().m_Temp_Tray_Info.bIsExist && GetPickLane().ArrivalSignal;
+        }
+
+        protected override BaseLane GetPickLane()
+        {
+            BaseLane pickLane = null;
+            switch (PPStation)
+            {
+                case clsEnum.PPStation.OK:
+                    if (m_pickPlace == PickPlace.Pick)
+                    {
+                        pickLane = Proc_OK_Lane.GetSingleton();
+                    }
+                    break;
+                case clsEnum.PPStation.NG:
+                    if (m_pickPlace == PickPlace.Pick)
+                    {
+                        pickLane = Proc_NG_Lane.GetSingleton();
+                    }
+                    break;
+                default:
+                    return null;
+            }
+
+            return pickLane;
+        }
+
+        protected override BaseLane GetPlaceLane()
+        {
+            BaseLane placeLane = null;
+            switch (PPStation)
+            {
+                case clsEnum.PPStation.OK:
+                    if (m_pickPlace == PickPlace.Place)
+                    {
+                        placeLane = Proc_OK_Lane.GetSingleton();
+                    }
+                    break;
+                case clsEnum.PPStation.NG:
+                    if (m_pickPlace == PickPlace.Place)
+                    {
+                        placeLane = Proc_NG_Lane.GetSingleton();
+                    }
+                    break;
+                default:
+                    return null;
+            }
+
+            return placeLane;
+        }
+
+        protected override bool ReadyToPlace()
+        {
+            return GetPlaceLane().m_Temp_Tray_Info.bIsExist && GetPlaceLane().ArrivalSignal;
+        }
+
+        protected override void TransferToLane()
+        {
+            var placeLane = GetPlaceLane();
+            if (placeLane == null)
+                return;
+
+            var index = placeLane.m_Temp_Tray_Info.GetIndexFromRowCol(m_iPlaceRow, m_iPlaceColumn);
+
+            AssyRecord.CopyTo(placeLane.m_Temp_Tray_Info.AssyRecords[index]);
+            var assyRecord = placeLane.m_Temp_Tray_Info.AssyRecords[index];
+            var itemStatus = m_Temp_Tray_Info.ConvertToItemStatus(assyRecord.AoiResult);
+            AssyRecord.IsExist = false;
+            placeLane.m_Temp_Tray_Info.SetItemStatus(index, itemStatus);
+        }
+    }
+}
